02b07fdf88e3fe80cf937efb00a532e4bcfc4787
Andrew M <matveev.andrew@internet.ru>
2023-01-28 19:25:06 +0600

    Задача 6 сделана

---
+++ Homework/HW_Задача_6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+
+// Пример:
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.Write("Insert the number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int div = a % 2;
+
+if (div == 0)
+{
+    System.Console.WriteLine("The number is even");
+}
+else
+{
+    System.Console.WriteLine("The number is odd");
+}
